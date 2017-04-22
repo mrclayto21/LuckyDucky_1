d0a1765190f4761515d234ed0baad47ddff5b10e
Misha Claytor <mclaytor@uncc.edu>
2017-04-22 01:41:38 +0000

    Added validation, not working properly

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ApplicationRecord
+    # validates :userName, presence: true
+    # validates :prime, presence: true
+    # validates :name, presence: true
+    # validates :prep, presence: true
+    # validates :complete, presence: true
+    # validates :ingredients, presence: true
+    # validates :directions, presence: true
+    # validates :secondary, presence: true
 end
