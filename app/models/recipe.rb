dd1c44860d60eec30c4274391be44d3fa62c3a2b
Misha Claytor <mclaytor@uncc.edu>
2017-05-07 18:45:21 +0000

    Added edit form functionality; fixed data pull nil issue

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,24 @@ class Recipe < ApplicationRecord
     validates :ingredients, presence: true
     validates :directions, presence: true
     validates :secondary, presence: true
+    
+    CATEGORY = [
+        ["Breakfast", 1], 
+        ["Lunch", 2], 
+        ["Dinner", 3], 
+        ["Dessert", 4], 
+        ["Drink", 5]
+    ]
+    
+    SECONDARY = [
+        ['-', 0], 
+        ['Gluten-free', 1],
+        ['Lactose-free', 2], 
+        ['Vegan', 3], 
+        ['Vegetarian', 4], 
+        ['Low Calorie', 5], 
+        ['Low Fat', 6], 
+        ['Diabetic', 7], 
+        ['Slow Cooker']
+    ]
 end
